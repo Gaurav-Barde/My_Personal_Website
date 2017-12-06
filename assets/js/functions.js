cc4c0d7494fb508e7e4055269ab230e36203c6ed
Gaurav Barde <gabarde25@gmail.com>
2017-12-06 15:19:34 +0530

    Initial Commit

---
+++ assets/js/functions.js
@@ -0,0 +1,10 @@
+window.onload = function(e) {
+  var aboutSection = document.querySelector('.about');
+
+  setTimeout(function() {
+
+    aboutSection.classList.add('make-visible');
+
+  }, 1000);
+
+}
